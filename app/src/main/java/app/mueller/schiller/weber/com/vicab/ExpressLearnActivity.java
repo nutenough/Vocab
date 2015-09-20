5b52aaebfb94f37c609d47c21d323d90c41b3276
Qoes <Qoes@hotmail.de>
2015-09-20 13:44:16 +0200

    Expressabfrage funktioniert! + Counter

--- app/src/main/java/app/mueller/schiller/weber/com/vicab/ExpressLearnActivity.java
+++ app/src/main/java/app/mueller/schiller/weber/com/vicab/ExpressLearnActivity.java
@@ -2,23 +2,19 @@ package app.mueller.schiller.weber.com.vicab;
 
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import app.mueller.schiller.weber.com.vicab.Database.DBAdmin;
 import app.mueller.schiller.weber.com.vicab.PersistanceClasses.VocItem;
-import app.mueller.schiller.weber.com.vicab.R;
 
 import static android.app.PendingIntent.getActivity;
 
@@ -28,8 +24,14 @@ public class ExpressLearnActivity extends AppCompatActivity {
     private RelativeLayout vocab_knowing_language;
     private TextView vocab_learning_language_text_view;
     private TextView vocab_knowing_language_text_view;
+    private Button vocab_counter_button;
     private boolean isBackVisible = false;
     private Handler handler;
+    private ArrayList<VocItem> allVocab;
+    private int counter = 0;
+    private int listSize;
+
+    private DBAdmin dbAdmin;
 
 
 
@@ -37,18 +39,49 @@ public class ExpressLearnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_express_learn);
+        initDB();
+        fillListFromDB();
+        initUIElements();
+        fillVocabCounter();
         setupVocabAnimation();
-
-
 
 
     }
 
-    private void setupVocabAnimation(){
+    private void fillVocabCounter() {
+        listSize = allVocab.size();
+        vocab_counter_button.setText("0/" + listSize);
+    }
+
+    private void updateCounter() {
+        counter++;
+        vocab_counter_button.setText(counter + "/" + listSize);
+    }
+
+    private void fillListFromDB(){
+        allVocab = new ArrayList<>();
+        allVocab.addAll(dbAdmin.getAllVocab());
+        Log.d("Learn", "allItems: " + allVocab);
+    }
+
+    private void initDB(){
+        dbAdmin = new DBAdmin(this);
+        dbAdmin.open();
+    }
+
+    protected void onDestroy() {
+        dbAdmin.close();
+        super.onDestroy();
+    }
+
+    private void initUIElements() {
         vocab_knowing_language = (RelativeLayout)findViewById(R.id.vocab_knowing_language);
         vocab_learning_language_text_view = (TextView)findViewById(R.id.vocab_learning_language_text_view);
         vocab_knowing_language_text_view =(TextView)findViewById(R.id.vocab_knowing_language_text_view);
+        vocab_counter_button = (Button) findViewById(R.id.vocab_counter);
+    }
 
+    private void setupVocabAnimation(){
 
         final AnimatorSet setRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),
                 R.animator.flip_right_out);
@@ -100,17 +133,21 @@ public class ExpressLearnActivity extends AppCompatActivity {
                     @Override
                     public void run() {
 
-
                         setRightOut_1.setTarget(vocab_learning_language_text_view);
                         setLeftIn_1.setTarget(vocab_knowing_language_text_view);
                         setRightOut_1.start();
                         setLeftIn_1.start();
                         isBackVisible = false;
 
-                        vocab_knowing_language_text_view.setText("wissen");
-                        vocab_learning_language_text_view.setText("to know");
-
-
+                        if (counter < listSize) {
+                            vocab_knowing_language_text_view.setText(allVocab.get(counter).getSourceVocab());
+                            vocab_learning_language_text_view.setText(allVocab.get(counter).getTargetVocab());
+                            updateCounter();
+                        }
+                        else {
+                            vocab_knowing_language_text_view.setText("Finished!");
+                            vocab_learning_language_text_view.setText("Great Job");
+                        }
 
 
 
