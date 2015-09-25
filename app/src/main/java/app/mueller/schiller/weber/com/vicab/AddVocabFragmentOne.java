e69818ccb15a866b8d0166c3ac1ebd2cb636400c
Qoes <Qoes@hotmail.de>
2015-09-25 17:22:32 +0200

    Spinner in AddVocab funktioniert aus DB

--- app/src/main/java/app/mueller/schiller/weber/com/vicab/AddVocabFragmentOne.java
+++ app/src/main/java/app/mueller/schiller/weber/com/vicab/AddVocabFragmentOne.java
@@ -7,15 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.mueller.schiller.weber.com.vicab.Database.DBAdmin;
 import app.mueller.schiller.weber.com.vicab.Database.ViCabContract;
+import app.mueller.schiller.weber.com.vicab.PersistanceClasses.ListItem;
 
-public class AddVocabFragmentOne extends Fragment {
+public class AddVocabFragmentOne extends Fragment implements
+        AdapterView.OnItemSelectedListener {
     View contentView;
     private Button vocabAddBTN;
     private Button vocabCancelBTN;
@@ -26,6 +34,7 @@ public class AddVocabFragmentOne extends Fragment {
     private String sourceVocab;
     private String targetVocab;
     private String vocabCouple;
+    private Spinner spinner;
 
     private DBAdmin dbAdmin;
 
@@ -44,30 +53,12 @@ public class AddVocabFragmentOne extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         setupUIComponents();
         initDB();
+        setOnClickListeners();
+        fillSpinnerFromDB();
     }
 
-    private void initDB() {
-        dbAdmin = new DBAdmin(getActivity());
-        dbAdmin.open();
-    }
 
-    public void onDestroy() {
-        dbAdmin.close();
-        super.onDestroy();
-    }
-
-    private void setupUIComponents() {
-        sourceVocabET = (EditText) getActivity().findViewById(R.id.sourceVocabET);
-        targetVocabET = (EditText) getActivity().findViewById(R.id.targetVocabET);
-
-        // Gets the source- and target-language
-        sourceVocabTV = (TextView) getActivity().findViewById(R.id.sourceVocabTV);
-        targetVocabTV = (TextView) getActivity().findViewById(R.id.targetVocabTV);
-        sourceVocabTV.setText(ViCabContract.CHOSEN_LANGUAGE_SOURCE);
-        targetVocabTV.setText(ViCabContract.CHOSEN_LANGUAGE_TARGET);
-
-        // Adds Vocab to Meine Vokabeln
-        vocabAddBTN = (Button) getActivity().findViewById(R.id.vocabAddBTN);
+    private void setOnClickListeners(){
         vocabAddBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,8 +73,6 @@ public class AddVocabFragmentOne extends Fragment {
             }
         });
 
-        // Cancel button
-        vocabCancelBTN = (Button) getActivity().findViewById(R.id.vocabCancelBTN);
         vocabCancelBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,6 +80,71 @@ public class AddVocabFragmentOne extends Fragment {
                 startActivity(intent);
             }
         });
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    private void fillSpinnerFromDB() {
+
+        // Spinner Drop down elements
+        List<ListItem> listItems = dbAdmin.getAllListsForChosenLanguage();
+        List<String> labels = new ArrayList<>();
+        labels.add(getResources().getString(R.string.spinner_no_list));
+        for (int i = 0; i <listItems.size(); i++) {
+            labels.add(listItems.get(i).getName());
+        }
+
+        // adapter for spinner
+        ArrayAdapter<ListItem> dataAdapter = new ArrayAdapter(getActivity(),
+                android.R.layout.simple_spinner_item, labels);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item do stuff
+
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
+    }
+
+
+    private void initDB() {
+        dbAdmin = new DBAdmin(getActivity());
+        dbAdmin.open();
+    }
+
+    public void onDestroy() {
+        dbAdmin.close();
+        super.onDestroy();
+    }
+
+    private void setupUIComponents() {
+        sourceVocabET = (EditText) getActivity().findViewById(R.id.sourceVocabET);
+        targetVocabET = (EditText) getActivity().findViewById(R.id.targetVocabET);
+        spinner = (Spinner) getActivity().findViewById(R.id.addListSpinner);
+
+        // Gets the source- and target-language
+        sourceVocabTV = (TextView) getActivity().findViewById(R.id.sourceVocabTV);
+        targetVocabTV = (TextView) getActivity().findViewById(R.id.targetVocabTV);
+        sourceVocabTV.setText(ViCabContract.CHOSEN_LANGUAGE_SOURCE);
+        targetVocabTV.setText(ViCabContract.CHOSEN_LANGUAGE_TARGET);
+
+        // Adds Vocab to Meine Vokabeln
+        vocabAddBTN = (Button) getActivity().findViewById(R.id.vocabAddBTN);
+
+        // Cancel button
+        vocabCancelBTN = (Button) getActivity().findViewById(R.id.vocabCancelBTN);
+
     }
 
     private void saveInput() {
@@ -102,3 +156,4 @@ public class AddVocabFragmentOne extends Fragment {
     }
 
 }
+
