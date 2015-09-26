16f100f77cbc16255cbfeaeb7bb125e43346545d
Qoes <Qoes@hotmail.de>
2015-09-26 17:30:43 +0200

    VocItem resetKnown statt decrease

--- app/src/main/java/app/mueller/schiller/weber/com/vicab/PersistanceClasses/VocItem.java
+++ app/src/main/java/app/mueller/schiller/weber/com/vicab/PersistanceClasses/VocItem.java
@@ -33,8 +33,8 @@ public class VocItem {
         known++;
     }
 
-    public void decreaseKnown() {
-        known--;
+    public void resetKnown() {
+        known = 0;
     }
 
     public int getAsked() {
