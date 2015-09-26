0c3831fe9ce67d5293f41de82d1272d6e62ef932
Christian Weber <weber.chris@hotmail.de>
2015-09-26 15:40:35 +0200

    Spinner für Wortart

--- app/src/main/java/app/mueller/schiller/weber/com/vicab/AddVocabFragmentTwo.java
+++ app/src/main/java/app/mueller/schiller/weber/com/vicab/AddVocabFragmentTwo.java
@@ -6,14 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RatingBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class AddVocabFragmentTwo extends Fragment {
+public class AddVocabFragmentTwo extends Fragment implements AdapterView.OnItemSelectedListener {
     View contentView;
     private RatingBar ratingBar;
     private String ratedValue;
     private TextView rateMessage;
+    private Spinner spinner;
+    private String[] wordClassArray = {"Substantiv", "Verb", "Adjektiv", "Sonstiges"};
 
     // The view
     @Nullable
@@ -21,7 +26,13 @@ public class AddVocabFragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         contentView = inflater.inflate(R.layout.add_vocab_fragment_two, null);
         setupUIComponents();
+        fillSpinner();
         return contentView;
+    }
+
+    private void setupUIComponents() {
+        ratingBar = (RatingBar) contentView.findViewById(R.id.ratingBar);
+        spinner = (Spinner) contentView.findViewById(R.id.wordClassSpinner);
     }
 
     // Page content ("findViewById" has to be "view.findViewById"; "getApplicationContext" has to be "getActivity()")
@@ -39,10 +50,29 @@ public class AddVocabFragmentTwo extends Fragment {
                 rateMessage.setText("Rating : " + ratedValue + "/3");
             }
         });
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
     }
 
-    private void setupUIComponents() {
-        ratingBar = (RatingBar) contentView.findViewById(R.id.ratingBar);
-        rateMessage = (TextView) contentView.findViewById(R.id.ratingMessage);
+    private void fillSpinner() {
+        // adapter for spinner
+        ArrayAdapter<?> dataAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, wordClassArray);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item do stuff
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
     }
 }
